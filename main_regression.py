8e049d4f30ce322bd3862607a1f0d0a932b862eb
Orous <orouskha@ualberta.ca>
2022-10-17 21:22:14 -0600

    final commit

--- main_regression.py
+++ main_regression.py
@@ -6,13 +6,10 @@ import sklearn.preprocessing as pp
 import sklearn.model_selection as ms
 import sklearn.decomposition as dec
 import matplotlib.pyplot as plt
-import seaborn as sb
 import pandas as pd
 from sklearn.metrics import mean_squared_error
-from sklearn.metrics import r2_score
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from plot_functions import result_lr, result_mlp, result_deep, real_map, est_mlp_map 
 
 
 def result(model, trX, teX, trY, teY):
@@ -80,9 +77,7 @@ df = pd.read_csv('data.csv', sep=',', header=0, encoding='utf-8')
 
 Y = df['SM'].to_numpy().reshape((-1, 1))
 df.drop(['SM'], inplace=True, axis=1)
-# df.drop(['Irrigation'], inplace=True, axis=1)
 X = df.to_numpy()
-# X = df['TVDI 3'].to_numpy().reshape((-1, 1))
  
 
 scalerX = pp.MinMaxScaler(feature_range=(0,1))
@@ -93,70 +88,59 @@ X = pca.fit_transform(X)
 
 
 trX, teX, trY, teY = ms.train_test_split(X, Y, train_size=0.8, random_state=0)
-## Fix random_state = 0
 
+
+
+
+
+
+""" Linear Regression Model """
+
+lr = li.LinearRegression()
+lr.fit(trX, trY)
+result(lr, trX, teX, trY, teY)
+
+trpred_lr = lr.predict(trX)
+tepred_lr = lr.predict(teX)
+
+
+
+
+""" NN (MLP) Regression Model """
 
 
 mlp = nn.MLPRegressor(hidden_layer_sizes=(300,300), activation='relu', 
                       batch_size=4, tol=1e-9, max_iter=500, solver='adam', random_state=0)
-# Fix random_state = 0 or 6.
-# Some good neurons: (200,50), (300,300), (50,30), (10)
+
+
 mlp.fit(trX, trY)
+
+
 result(mlp, trX, teX, trY, teY)
 
 
-# lr = li.LinearRegression()
-# lr.fit(trX, trY)
-# result(lr, trX, teX, trY, teY)
-
 """ MLP Features """
-# mlp.n_iter_
 
-# #epochs1 = range(1,mlp.n_iter_+1)
-# #a = mlp.loss_curve_
-# #plt.figure()
-# #plt.plot(epochs1,mlp.loss_curve_)
+mlp.n_iter_
 
-
-# weights = mlp.coefs_
-# biases = mlp.intercepts_
-
-
-
-
-# """ MLP Features """ 
-
-trpred = mlp.predict(trX)
-tepred = mlp.predict(teX)
-
-# trpred = lr.predict(trX)
-# tepred = lr.predict(teX)
-
-
-# n1 = 300
-# n2 = 300
-# deep = Sequential()
-# deep.add(Dense(n1, input_dim=4, activation= "relu")) ## First Hidden Layer has n1 neurons 
-# deep.add(Dense(n2, activation= "relu")) ## Second Hidden Layer has n2 neurons 
-# deep.add(Dense(1, activation='sigmoid')) ## neurons in last layer is the same as output Dim
-# deep.summary() #Print model Summary
-# deep.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
-# deep.fit(trX, trY, epochs=50)
-# result_deep(deep, trX, teX, trY, teY)
-
-
-# ## Evaluate the Accuracy of Deep Neural Model
-# [loss, accuracy_training] = deep.evaluate(trX, trY)
-# print('Accuracy for training data: %.2f' % (accuracy_training*100))
-
-# _, accuracy_testing = deep.evaluate(teX, teY)
-# print('Accuracy for testing data: %.2f' % (accuracy_testing*100))
-
-
-# plt.rcParams["figure.figsize"] = (10,6)
-# plt.figure()
+epochs1 = range(1,mlp.n_iter_+1)
+a = mlp.loss_curve_
 plt.figure()
-plt.plot(tepred, lw=1.5, label='Prediction' )
+plt.plot(epochs1,mlp.loss_curve_)
+
+
+weights = mlp.coefs_
+biases = mlp.intercepts_
+
+
+
+trpred_mlp = mlp.predict(trX)
+tepred_mlp = mlp.predict(teX)
+
+
+
+plt.figure()
+plt.plot(tepred_mlp, lw=1.5, label='Prediction' )
 plt.plot(teY, lw=1.5, label='Real')
 plt.title('Prediction on Testing Data')
 plt.xlabel('Samples')
@@ -166,13 +150,40 @@ plt.savefig('Testing.jpg',dpi=300)
 
 
 plt.figure()
-plt.plot(trpred, lw=1.2, label='Prediction' )
+plt.plot(trpred_mlp, lw=1.2, label='Prediction' )
 plt.plot(trY, lw=1.2, label='Real')
 plt.title('Prediction on Training Data')
 plt.xlabel('Samples')
 plt.ylabel('Soil Moisture')
 plt.legend()
 plt.savefig('Training.jpg',dpi=300)
+
+
+
+
+
+
+
+""" Deep Neural Regression Model Using TensorFlow"""
+
+n1 = 300
+n2 = 300
+deep = Sequential()
+deep.add(Dense(n1, input_dim=4, activation= "relu")) ## First Hidden Layer has n1 neurons 
+deep.add(Dense(n2, activation= "relu")) ## Second Hidden Layer has n2 neurons 
+deep.add(Dense(1, activation='sigmoid')) ## neurons in last layer is the same as output Dim
+deep.summary() #Print model Summary
+deep.compile(loss= "mean_squared_error" , optimizer="adam", metrics=["mean_squared_error"])
+deep.fit(trX, trY, epochs=50)
+
+
+## Evaluate the Accuracy of Deep Neural Model
+[loss, accuracy_training] = deep.evaluate(trX, trY)
+print('Accuracy for training data: %.2f' % (accuracy_training*100))
+
+_, accuracy_testing = deep.evaluate(teX, teY)
+print('Accuracy for testing data: %.2f' % (accuracy_testing*100))
+
 
 
 
